4ce8e4ea3fd394929161ff9d0a4f70e89ef2df2d
wwwwolf <wwwwolf@iki.fi>
2006-12-03 15:11:45 +0200

    Icon.
    darcs-hash:20061203131145-4ca05-076bf5ca1e9b081a3f47b5fb296122cfee357ea1.gz

--- conmandictionary.sh
+++ conmandictionary.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# $Id: conmandictionary.sh 11 2006-12-02 15:27:57Z wwwwolf $
+# $Id: conmandictionary.sh 12 2006-12-03 13:11:45Z wwwwolf $
 
+declare currdir=`dirname $0`
 exec java -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel \
-	-jar conmandictionary.jar $1
+	-jar "$currdir/conmandictionary.jar" $1
 

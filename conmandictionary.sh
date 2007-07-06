48fa9824d8e634a1c25593215cb210b60bed1c66
wwwwolf <wwwwolf@iki.fi>
2007-07-06 23:31:49 +0300

    Made the shell script to work in Dash
    ...also removed one svn'ism. For some reason, Dash doesn't get
    "declare".
    
    darcs-hash:20070706203149-36ee1-c54f429b01ed9bf99935842120d75583786ea1fc.gz

--- conmandictionary.sh
+++ conmandictionary.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-# $Id: conmandictionary.sh 12 2006-12-03 13:11:45Z wwwwolf $
-
-declare currdir=`dirname $0`
+currdir=`dirname $0`
 exec java -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel \
 	-jar "$currdir/conmandictionary.jar" $1
 

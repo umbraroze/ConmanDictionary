c499d640abeea30830a41e8bc46af8c670ffb599
Urpo Lankinen <wwwwolf@iki.fi>
2016-07-01 15:26:55 +0300

    Qt Creator kinda sucks. Never mind

--- conmandictionary/dictionary_data.cpp
+++
@@ -1,9 +0,0 @@
-/*********************************************************************
- *
- * Method implementation for dictionary data classes defined in
- * "dictionary_data.h".
- *
- ********************************************************************/
-
-#include "dictionary_data.h"
-

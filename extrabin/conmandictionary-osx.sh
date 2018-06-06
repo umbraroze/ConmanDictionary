610844456eff602e7b62cd640e39109b44ed8751
Urpo Lankinen <wwwwolf@iki.fi>
2018-06-06 13:58:13 +0300

    Removed useless Java stuff, moved Java version code to 'obsolete'

--- extrabin/conmandictionary-osx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# For running Conman's Dictionary on OS X.
-java \
-  -Dapple.laf.useScreenMenuBar=true \
-  -Xdock:name="Conman's Dictionary" \
-  -jar ConmanDictionary.jar
--- extrabin/conmandictionary-win.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-javaw -Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel -jar conmandictionary.jar %1
-
